d5f0b23414b8391f80cfe18436a7525ace2a541f
Dino Mariano <61036925+d1lm@users.noreply.github.com>
2020-11-02 20:56:05 -1000

    finshed digits 1

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const divStyle = { paddingTop: '15px' };
+    const divStyle = { paddingTop: '15px', color: 'black' };
     return (
-        <footer>
+        <footer color='white'>
           <div style={divStyle} className="ui center aligned container">
             <hr />
               Department of Information and Computer Sciences <br />
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,39 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Icon, Header } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
+     <div className='digits-landing-background'>
+        <Grid container centered stackable columns={3}>
 
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
+          <Grid.Column textAlign='center'>
+            <Icon name='users' size='huge'></Icon>
+            <Header as='h1'>Multiple Users</Header>
+            <p>
+              This address book enables any number of users to register and sace their business contacts. You can only see the contacts you have created.
+            </p>
           </Grid.Column>
 
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
+          <Grid.Column textAlign='center'>
+            <Icon name='file alternate outline' size='huge'></Icon>
+            <Header as='h1'>Contact Details</Header>
+            <p>
+              For each contact, you can save their name, address, and phone number.
+            </p>
+          </Grid.Column>
+
+          <Grid.Column textAlign='center'>
+            <Icon name='calendar' size='huge'></Icon>
+            <Header as='h1'>Timestamped Notes</Header>
+            <p>
+              Each time you make contact with a contact, you can write a note that summarized thje conversation. This note is saved along with a timestamp with the   contact.
+            </p>
           </Grid.Column>
 
         </Grid>
+     </div>
     );
   }
 }
